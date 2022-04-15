43e243ca4bd51164de03cd135a1bbe30e9b87106
unknown <qoo40328@gmail.com>
2022-04-15 23:47:01 +0800

    p1

--- 1091420_proj_1.asm
+++ 1091420_proj_1.asm
@@ -6,7 +6,6 @@
 .text
 
 main:
-	
 	li x8, MT
 	la a0, Input
 	li a7, 4 		    #print Input0 string
@@ -14,9 +13,7 @@ main:
 	li a7,5 		    #read input
 	ecall
 	bge a0, x8, End		#x>=100的話就結束
-	
 	jal f
-	
 	mv t0, a0		
 	la a0, Output
 	li a7, 4
@@ -43,72 +40,55 @@ f:
 R1:
 	addi a0, x0, 1
 	ret
-	
 R5:
 	addi a0, x0, 5
 	ret
-	
 R6:
 	addi a0, x0, -1
-	ret	
-	
+	ret		
 R21:
 	addi x2, x2, -16	# 開stack
 	sw a0, 0(x2)		# 存a0
 	sw x1, 8(x2)		#存x1
-	
 	addi a0, a0, -1
 	jal f		        #f(x-1)
-	
 	lw t1, 0(x2)		#get  x
 	sw a0, 0(x2)		#store f(x-1) 
 	addi a0, t1, -2
 	jal f		        #跳f(x-2)
-	
 	lw t1, 0(x2)		#get f(x-1)
 	add a0, a0, t1		#f(x-2) + f(x-1)
-	
 	lw x1, 8(x2)		#要X1
 	addi x2, x2, 16	    #還原stack
 	ret
-	
 	
 R23:
 	addi x2, x2, -16	# 開stack
 	sw a0, 0(x2)		# 存a0
 	sw x1, 8(x2)		#存x1
-	
 	addi a0, a0, -3
 	jal f		        #f(x-3)
-	
 	lw t1, 0(x2)		#get  x
 	sw a0, 0(x2)		#store f(x-3) 
 	addi a0, t1, -2
 	jal f		        #f(x-2)
-	
 	lw t1, 0(x2)		#get f(x-3)
 	add a0, a0, t1		#f(x-2) + f(x-3)
-	
 	lw x1, 8(x2)		#要X1
 	addi x2, x2, 16	    #還原stack
 	ret
-	
 	
 RD5:
 	addi x2, x2, -16    # 開stack
 	sw a0, 0(x2)		# 存a0
 	sw x1, 8(x2)		#存x1
-	
 	addi t1, x0, 5
 	div a0, a0, t1		#f(x/5)
 	jal f
-	
 	lw t0, 0(x2)		#get x
 	addi t1, x0, 2
 	mul t0, t0, t1		#2*x
-	
 	add a0, a0, t0		#把值給回a0
-	
 	lw ra, 8(x2)		#要X1
 	addi x2, x2,16	    #還原stack
 	ret
